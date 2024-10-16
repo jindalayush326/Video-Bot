7d91b9d2b6a514bc63e87525958b3d1bde7ff505
Ayush Jindal <104627503+jindalayush326@users.noreply.github.com>
2024-10-16 23:33:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,22 +72,3 @@ def process_video(uploaded_file):
         answer = chain.run(input_documents=context, question=question)
         st.session_state.questions.append(answer)
 
-# Loop until five questions are generated
-# while len(st.session_state.questions) < 5:
-#     unique_key = f"uploader_{st.session_state.upload_count}"
-#     uploaded_file = st.file_uploader("Upload your video file", type=["mp4", "mov", "avi"], key=unique_key)
-
-#     if uploaded_file is not None:
-#         st.video(uploaded_file)
-#         process_video(uploaded_file)
-
-#         if st.session_state.questions:
-#             st.write("Generated Question: ", st.session_state.questions[-1])
-#         else:
-#             st.write("No question was generated.")
-        
-#         st.session_state.upload_count += 1  
-
-# # After 5 questions, inform the user
-# if len(st.session_state.questions) >= 5:
-#     st.write("You have generated 5 questions. Thank you for using the application!")
